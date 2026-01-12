8b2f744d43bc1ae5ebd7c392dc8aafa8e114f448
J Chiranjevi Anil <jadachiranjevianil340@gmail.com>
2026-01-12 22:32:48 +0530

    Hacker Rank Day 4 Challenge

---
+++ SDE PROGRAMS/src/problem_Solving/C11.java
@@ -0,0 +1,31 @@
+package problem_Solving;
+// Hacker Rank Challenge 11
+import java.util.*;
+class Hrc11{
+	void input() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter Number :: ");
+		int num = scanner.nextInt();
+		productOfDigits(num);
+		scanner.close();
+	}
+	static void productOfDigits(int num) {
+		int product = 1;
+		while(num!=0) {
+			int rem  = num % 10;
+			product *= rem;
+			num /= 10;
+		}
+		display(product);
+	}
+	static void display(int result) {
+		System.out.println(result);
+	}
+}
+public class C11 {
+	public static void main(String[] args) {
+		Hrc11 c = new Hrc11();
+		c.input();
+	}
+
+}
