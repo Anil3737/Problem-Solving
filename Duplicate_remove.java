da3b4b964f307366abacd0f57f37316c8393572a
J.Chiranjevi Anil <jadachiranjevianil340@gmail.com>
2026-01-14 21:55:24 +0530

    Add files via upload

---
+++ Duplicate_remove.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Duplicate_remove {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Number:: ");
+		int size = scanner.nextInt();
+		int[] arr = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = scanner.nextInt();
+		}
+		for (int i = 0; i < size; i++) {
+			for (int j = i + 1; j < size; j++) {
+				if (arr[i] == arr[j]) {
+				
+				}
+			}
+		}
+		for (int i = 0; i < size; System.out.println(arr[i]), i++)
+			;
+		scanner.close();
+	}
+
+}
