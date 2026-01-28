894700ed51cb2b2bc8f8a4be8172d613650df8ad
J.Chiranjevi Anil <jadachiranjevianil340@gmail.com>
2026-01-28 21:18:32 +0530

    Refactor Happy class methods and improve checks

--- SDE PROGRAMS/src/problem_Solving/C22.java
+++ SDE PROGRAMS/src/problem_Solving/C22.java
@@ -1,39 +1,33 @@
-import java.io.*;
 import java.util.*;
 
+class Happy {
 
-class Happy{
-    static int sum(int n){
-        int s=0,d;
-        while(n!=0){
+    static int sum(int n) {
+        int s = 0, d;
+        while (n != 0) {
             d = n % 10;
-            s = s+(d*d);
+            s += d * d;
             n /= 10;
         }
         return s;
     }
-    static void check(int num){
-        while(num!=0){
-            if(num>0 && num<9){
-                if(num==1){
-                    System.out.println("true");
-                    break;
-                }else
-                    System.out.println("false");
-            }            
+
+    static void check(int num) {
+        while (num != 1 && num != 4) {
+            num = sum(num);   // update num
         }
-        sum(num);
+
+        if (num == 1)
+            System.out.println("true");
+        else
+            System.out.println("false");
     }
 }
 
 public class Solution {
-
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner obj = new Scanner(System.in);
         int num = obj.nextInt();
-        Happy h = new Happy();
-        h.check(num);
-        
+        Happy.check(num);   // static call
     }
 }
