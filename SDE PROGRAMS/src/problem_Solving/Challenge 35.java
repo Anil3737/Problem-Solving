2a001930d18deb4f0f95a6d9aec393b1ed3a642e
J.Chiranjevi Anil <jadachiranjevianil340@gmail.com>
2026-02-02 20:36:32 +0530

    Add Challenge 35 to count multiples of 5
    Implement a program to count multiples of 5 in an array.

---
+++ SDE PROGRAMS/src/problem_Solving/Challenge 35.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int arr[] = new int[num];
+        int count=0;
+        for(int i=0;i<num;i++){
+            arr[i] = scanner.nextInt();
+        }
+        for(int i:arr){
+            if(i%5==0){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
