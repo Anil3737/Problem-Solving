683566cfe65d64471784ff8538eaecfba89c8a0a
J.Chiranjevi Anil <jadachiranjevianil340@gmail.com>
2026-02-03 21:43:06 +0530

    Add input handling and StringBuilder initialization

--- SDE PROGRAMS/src/problem_Solving/Challenge 32.java
+++ SDE PROGRAMS/src/problem_Solving/Challenge 32.java
@@ -2,5 +2,8 @@ import java.util.*;
 public class Solution{
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
+    String str = scanner.next();
+    StringBuilder result = new StringBuilder();
+    
   }
 }
