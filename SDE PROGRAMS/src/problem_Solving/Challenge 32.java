8c2d6a335b644e274586e541d10081806e8b4113
J.Chiranjevi Anil <jadachiranjevianil340@gmail.com>
2026-02-03 21:47:38 +0530

    Fix character removal logic in Challenge 32
    Fix loop increment and correct method name for character comparison.

--- SDE PROGRAMS/src/problem_Solving/Challenge 32.java
+++ SDE PROGRAMS/src/problem_Solving/Challenge 32.java
@@ -4,6 +4,20 @@ public class Solution{
     Scanner scanner = new Scanner(System.in);
     String str = scanner.next();
     StringBuilder result = new StringBuilder();
-    
+    for(int i=0;i<str.length();i++){
+      char ch = str.charAt(i);
+      if(result.length()>0 && result.chatAt(result.length()-1) == ch){
+        result.deleteCharAt(result.length()-1);
+      }
+      else{
+        result.append(ch);
+      }
+    }
+    if(result.length()==0){
+      System.out.println("Empty String");
+    }
+    else{
+      System.out.println(result.toString());
+    }
   }
 }
